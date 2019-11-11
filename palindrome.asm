3a39284415889721866797c6cbc71d0a4803e04a
adrian-deloera <54675158+adrian-deloera@users.noreply.github.com>
2019-11-11 12:01:04 -0600

    Added rest of features
    Need to fix sometimes registering enter key as input

--- palindrome.asm
+++ palindrome.asm
@@ -1,14 +1,21 @@
 // Nand2Tetris Palindrome Code
+// Adrian DeLoera  113372061
 
 @0
 M=1 // Set answer bit to the neutral value
 
-(START)
+@19
+D=A
 @begCur
-M=0
-@strCur //Set the string cursor equal to 0
+M=D
+
+@endCur
 M=0
 
+@18
+D=A
+@strCur 
+M=D
 
 (INPUTLOOP)
 @128  //jumps to compute when enter is pressed
@@ -18,29 +25,68 @@ D=D-M
 @COMPUTE
 D;JEQ
 
-@KBD  //calls the increment cursor when non-enter key is pressed
+@KBD
 D=M
-@INCREMENTCURSOR
+@ADDCHAR //calls the character adder when non-enter key is pressed
 D;JNE
-
 @INPUTLOOP
 0;JMP
 
-(INCREMENTCURSOR) //increments the cursor by 1
+(ADDCHAR) //increments the cursor by 1 and writes the keyboard input to memory
 @strCur
-D=M+1
+M=M+1
+A=M
 M=D
-A=D+A
-M=-1
+@WAITRELEASE
+0;JMP
 
+(WAITRELEASE) //Waits for the key to be released before continuing to avoid zeros being placed between values
+@KBD
+D=M
 @INPUTLOOP
+D;JEQ
+@WAITRELEASE
 0;JMP
 
 (COMPUTE) // checks whether or not it is a Palindrome
+@strCur
+D=M
+@endCur
+M=D
+A=D
+D=M
+@begCur
+A=M
+D=D-M
+
+@FAIL
+D;JNE
+
+@endCur
+M=M-1
+@begCur
+M=M+1
+@CHECKCURSORS
+D;JEQ
+
+(CHECKCURSORS) // checks position of cursors during computation
+@endCur
+D=M
+@begCur
+D=D-M
+@PASS
+D;JLE
+@COMPUTE
+D;JNE
+
+(FAIL) //sets answer value to false, and returns to the input loop.
 @0
 M=0
-
-(END) // end loop for testing
-@END
+@WAITRELEASE
 0;JMP
 
+(PASS)
+@0
+M=-1
+@WAITRELEASE
+0;JMP
